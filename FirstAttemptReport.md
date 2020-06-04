44b6ae82e7dfdbd30d3658d1e633fed095196e2f
rbartelme <r.p.bartelme@gmail.com>
2020-06-04 23:27:23 +0000

    First attempt at full network in FirstAttemptReport.md, and modified bnlearn_test.R

---
+++ FirstAttemptReport.md
@@ -0,0 +1,27 @@
+## First Static Network Attempt Report:
+
+---
+
+### Rationale:
+
+* First attempts at automated learning were with mixed results.
+
+* It's probably best to think about the biology and physics of the system, and hardcode a graph structure. Then it will be easier to fit the raw data to the graph structure and see what the probabilities are at each node.
+
+---
+
+### *Using informed decisions*
+
+* It may be best to actually link derived data
+
+* Important to link static properties of the system 
+
+#### Examples: 
+
+* Cultivars are not moving, they are statically linked to their location
+
+* Canopy height is linked to vapor pressure deficit (Poor, 2019)
+
+* min/max/daily mean are linked...but maybe choosing the mean value would be best?
+
+* The data are ordinal, that is important to specify - maybe it's important to make the time connection now?
--- bnlearn_test.R
+++ bnlearn_test.R
@@ -27,23 +27,25 @@ data2include<-c("range", "column", "cultivar", "canopy_height",  "vpd_mean", "da
 #subset data by variables to include
 s4clean<-as.data.frame(s4test[, colnames(s4test) %in% data2include])
 #make dummy vars for cultivar, with a data frame of character/numeric values
-cultivar_numeric<-as.data.frame(cbind(as.character(unique(s4clean$cultivar)),as.numeric(unique(s4clean$cultivar))))
+#cultivar_numeric<-as.data.frame(cbind(as.character(unique(s4clean$cultivar)),as.numeric(unique(s4clean$cultivar))))
 
 #make colnames match
-names(cultivar_numeric)<-c("cultivar", "num_cultivar")
+#names(cultivar_numeric)<-c("cultivar", "num_cultivar")
 
 #tibble conversion
-cultivar_numeric<-as_tibble(cultivar_numeric)
-s4clean<-as_tibble(s4clean)
+#cultivar_numeric<-as_tibble(cultivar_numeric)
+#s4clean<-as_tibble(s4clean)
 
 #join/drop columns for cultivar names
-s4_bnready<-left_join(s4clean, cultivar_numeric, by = 'cultivar') %>% select(-cultivar)
+#s4_bnready<-left_join(s4clean, cultivar_numeric, by = 'cultivar') %>% select(-cultivar)
 
 #convert to dataframe
-s4_bnIN <- as.data.frame(s4_bnready)
+#s4_bnIN <- as.data.frame(s4_bnready)
 
 #convert everything in data frame to a factor for bnlearn interoperability
-s4_bnIN[] <- lapply(s4_bnIN, as.factor)
+s4clean[] <- lapply(s4clean, as.factor)
+
+#s4_bnIN[] <- lapply(s4_bnIN, as.factor)
 
 #================================================================
 # 2.) Structure Learning (DAG building)
@@ -54,13 +56,13 @@ s4_bnIN[] <- lapply(s4_bnIN, as.factor)
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 # wind weather data
-wind_mat<-t(combn(grep("*wind_*", colnames(s4_bnIN), value = TRUE), m = 2))
+wind_mat<-t(combn(grep("*wind_*", colnames(s4clean), value = TRUE), m = 2))
 
 # relative humidity
-hum_mat <- t(combn(grep("rh_*", colnames(s4_bnIN), value = TRUE), m = 2))
+hum_mat <- t(combn(grep("rh_*", colnames(s4clean), value = TRUE), m = 2))
 
 # air temperature
-air_mat <- t(combn(grep("*air_*", colnames(s4_bnIN), value = TRUE), m = 2))
+air_mat <- t(combn(grep("*air_*", colnames(s4clean), value = TRUE), m = 2))
 
 #blacklist derived data in matrix
 bl <- rbind(wind_mat, hum_mat, air_mat)
@@ -68,30 +70,34 @@ bl <- rbind(wind_mat, hum_mat, air_mat)
 colnames(bl) <- c("from", "to")
 
 #whitelist a priori links
-wl <- as.matrix(c("daily_gdd", "canopy_height"), nrow = 1, ncol = 2)
+wl <- t(as.matrix(combn(c("daily_gdd", "canopy_height", "cultivar", "column", "range"),  m= 2 )))
 #add colnames to wl
 colnames(wl) <- c("from", "to")
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# Hybrid Structure Learning Algorithms #
+#  Structure Learning Algorithms       #
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # structure learning was running so rapidly because all commands lack restrict and maximize args
-
-
+s4_tabu <- tabu(s4clean, whitelist = wl, blacklist = bl, tabu = 15, max.iter = 1000)
+plot(s4_tabu)
 #rsmax2
 # a general implementation of the sparse candidate algorithm
-s4_rsmax2 <- rsmax2(s4_bnIN, blacklist = bl, restrict = "si.hiton.pc", restrict.args = list(cluster = cl, B = 10000, blacklist = bl, whitelist = wl, undirected = TRUE),
-  maximize = "hc", maximize.args = list(whitelist = wl, blacklist = bl, perturb = 20), debug = TRUE)
-
+# s4_rsmax2 <- rsmax2(s4_bnIN, blacklist = bl, whitelist = wl, restrict = "si.hiton.pc",  maximize = "hc", debug = TRUE)
+s4_rsmax2 <- rsmax2(s4_bnIN, blacklist = bl, whitelist = wl, restrict = "si.hiton.pc", maximize = "tabu", test = "zf", alpha = 0.01, score = "bic-g", debug = TRUE)
+plot(s4_rsmax2)
+ 
 #mmhc
 # a general implementation of min-max hill climbing
 
-s4_mmhc <- mmhc(s4_bnIN, blacklist = bl, debug = TRUE)
+s4_mmhc <- mmhc(s4_bnIN, blacklist = bl, whitelist = wl, debug = TRUE)
+
+plot(s4_mmhc)
 
 #h2pc
 #
-s4_h2pc <- h2pc(s4_bnIN, blacklist = bl, debug = TRUE)
+s4_h2pc <- h2pc(s4_bnIN, blacklist = bl, whitelist = wl, debug = TRUE)
 
+plot(s4_h2pc)
 
 #================================================================
 # 3.) Parallel parameter learning (fitting data to DAG)
