356fe80e37bb35c5d98cdaa7f2ab3b7425cf726d
Ryan Bartelme <r.p.bartelme@gmail.com>
2020-10-13 17:43:18 -0700

    added stan installs/package, brms, and more bayesian goodies

---
+++ rstudio-bayes-cpu/install_stan.R
@@ -0,0 +1,19 @@
+# Following instructions from
+# https://github.com/stan-dev/rstan/wiki/Installing-RStan-on-Linux
+# and https://github.com/antoine-sachet/rocker-ML/
+
+print("Running install_stan.R")
+
+## Creating /home/rstudio/.R/Makevars
+
+dotR <- file.path(Sys.getenv("HOME"), ".R")
+if (!file.exists(dotR)) dir.create(dotR)
+M <- file.path(dotR, "Makevars")
+if (!file.exists(M)) file.create(M)
+cat("\nCXX14FLAGS=-O3 -march=native -mtune=native -fPIC",
+    "CXX14=clang++",
+    file = M, sep = "\n", append = TRUE)
+
+## Installing rstan
+
+install.packages("rstan", type = "source")
