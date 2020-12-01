0fa422d61aa2ad6b63a4a6dd94d06814a552078b
rbartelme <r.p.bartelme@gmail.com>
2020-12-01 19:25:55 +0000

    added test for loop for estimating growth rate and max growth for all cultivars in a season

--- hmc_canopy_height_model.R
+++ hmc_canopy_height_model.R
@@ -8,6 +8,7 @@ set.seed(5)
 season6 <- na.omit(read.table(file = "~/phenophasebbn/season6_combined", sep = "\t", header = TRUE,
            stringsAsFactors = FALSE))
 
+
 #randomly sample 1 cultivar from the season for testing model
 s6_cultivars <- sample(unique(season6$cultivar), size = 1)
 
@@ -32,35 +33,57 @@ s6_subset <- s6_subset[order(as.Date(s6_subset$date), s6_subset$sitename),]
 # 
 # summary(fit1)
 # 
-sorg_priors2 <- prior(gamma(2, 2), lb = 0, nlpar = "b") +
-  prior(normal(350,50), nlpar = "c")
+
+
+#single cultivar debug
+sorg_priors2 <- prior(gamma(2, 2), lb = 0.01, nlpar = "b") +
+  prior(gamma(130, 0.35), lb = 300, nlpar = "c")
 
 fit2 <- brm(bf(canopy_height ~ c / (1 + exp(b * gdd)),
           b + c ~ 1, nl = TRUE),
           data = s6_subset, prior = sorg_priors2,
-          control = list(adapt_delta = 0.85, stepsize = 0.5),
-          cores = 4, thin = 5, iter = 100000)
-# 
+          control = list(adapt_delta = 0.99),
+          cores = 4, thin = 5, iter = 20000, seed = 42)
+#
 summary(fit2)
+# 
 
-# sorg_priors3 <- prior(normal(10, 5), nlpar = "a") +
-#                  prior(gamma(2, 2), lb = 0, nlpar = "b") +
-#                  prior(normal(350,50), nlpar = "c")
-# 
-# fit3 <- brm(bf(canopy_height ~ c / (1 + exp(a + b * gdd)), 
-#                a + b + c ~ 1, nl = TRUE),
-#             data = s6_subset, prior = sorg_priors3,
-#             control = list(adapt_delta = 0.9, stepsize = 0.5), iter = 10000)
-# 
-# pairs(fit3)
-# plot(fit3)
-# summary(fit3)
+
 # storing the summary as a list of tables, one can extract the estimated values
 # for a, b, and c sorg_fit_sum$fixed[,1]
-#make dataframe to populate with a, b, c, and esimated inflection point output
-# (c - a)/2
+#make dataframe to populate with b, c, and estimated inflection point (c/2) output
+# c/2
 # would equivalently be something like:
 # (fit1_summary$fixed[3,1]-fit1_summary$fixed[1,1])/2
 
-# for loop for cultivars
 
+#make season6 dataframe into a list of data frames for each cultivar
+s6_cultivar_list <- split(season6, season6$cultivar, drop = TRUE)
+
+#hmc output for each cultivar
+hmc_out <- data.frame(cultivar = character(),
+                      est_growth_rate = numeric(), 
+                      est_max_growth = numeric(),
+                      measured_max_growth = numeric(),
+                      sd_est_meas_max_growth = numeric(),
+                      est_inflection_point = numeric(),
+                      stringsAsFactors=FALSE) 
+
+gen_sorg_priors <- prior(gamma(2, 2), lb = 0.01, nlpar = "b") +
+  prior(gamma(130, 0.35), lb = 300, nlpar = "c")
+
+for(i in 1:length(s6_cultivar_list)){
+  hmc_out[i,1] <- names(s6_cultivar_list)[[i]] #store cultivar name
+  #run brms for cultivar with generalized priors
+  model_fit <- brm(bf(canopy_height ~ c / (1 + exp(b * gdd)),
+            b + c ~ 1, nl = TRUE),
+         data = s6_cultivar_list[[i]], prior = gen_sorg_priors,
+         control = list(adapt_delta = 0.99),
+         cores = 4, thin = 5, iter = 20000, seed = 42)
+  model_fit_sum <- summary(model_fit) #get model fit summary
+  hmc_out[i,2] <- model_fit_sum$fixed[1,1] #extract estimated growth rate
+  hmc_out[i,3] <- model_fit_sum$fixed[2,1] #extract estimated height
+  hmc_out[i,4] <- max(s6_cultivar_list[[i]]$canopy_height) #measured max height
+  hmc_out[i,5] <- sd(hmc_out[i,3], hmc_out[i,4]) #sd of heights
+  hmc_out[i,6] <- hmc_out[i,3]/2 #estimated inflection point 
+}
