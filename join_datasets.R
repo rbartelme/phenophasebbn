4d8b549a96083ccdb0e52148c24e7aefb4450651
Ryan Bartelme <r.p.bartelme@gmail.com>
2021-07-01 11:57:29 -0700

    added pseudocode to combine data with R

---
+++ join_datasets.R
@@ -0,0 +1,16 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+# load seasonal data from first preprocessing step
+
+# load relative growth rates from seasons 4 and 6 at MAC
+
+# load relative abundance of SNPs by gene per cultivar
+
+# join datasets
+
+# export combined data for use with causalnex python library
+
+
+
