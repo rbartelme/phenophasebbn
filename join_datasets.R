ffb51ccb7c91fd043b027429a2efff68d6693286
Ryan Bartelme <r.p.bartelme@gmail.com>
2021-07-15 07:21:46 -0700

    mvp join for causalnex input rgr_snp_joined.csv

--- join_datasets.R
+++ join_datasets.R
@@ -1,26 +1,25 @@
 #!/usr/bin/env Rscript
-
 library(tidyverse)
-library(wavelets)
 
-# load seasonal data from first preprocessing step
-s4_df <- read.table(file = "~/phenophasebbn/season4_combined.txt", sep = "\t", header = TRUE,
-           stringsAsFactors = FALSE)
-
-plot(x = s4_df$day_of_year, y = s4_df$vpd_mean)
-s4_vpd_fft <- fft(z = c(s4_df$day_of_year,s4_df$vpd_mean), inverse = TRUE)
 # load relative growth rates from seasons 4 and 6 at MAC
 rgr <- read.table(file = "~/phenophasebbn/jags/mac_growth_rate_modeled.csv",
            sep = ",", header = TRUE, stringsAsFactors = FALSE)
 # load relative abundance of SNPs by gene per cultivar
 snp_rel <- as.data.frame(t(as.matrix(read.table(file = "~/phenophasebbn/genomic_preprocessing/genewise_snp_relative_abundance.txt",
                       sep = "\t", header = TRUE, stringsAsFactors = FALSE))))
-
+#rename var to do join
 snp_rel_geno <- rownames_to_column(snp_rel, "genotype")
 
 # join datasets
 
-# export combined data for use with causalnex python library
+#subset relative growth rate modeling 
+rgr_sub <- rgr %>% 
+          select(season, genotype, max_height_cm)
+# join with snp data
+joined_pheno <- left_join(snp_rel_geno, rgr_sub, by = "genotype")
 
+# export combined data for use with causalnex python library
+write_csv(joined_pheno, file = "~/phenophasebbn/rgr_snp_joined.csv",
+                    col_names = TRUE)
 
 
