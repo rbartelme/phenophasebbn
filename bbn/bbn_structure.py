0ae9f0e1b1f8f9e5f40868f285506ef63d4af44b
rbartelme <r.p.bartelme@gmail.com>
2021-10-22 12:13:18 -0700

    added pseudocode to generate expert informed DAG

--- bbn/bbn_structure.py
+++ bbn/bbn_structure.py
@@ -51,30 +51,62 @@ with open("/work/phenophasebbn/bbn/genotype_map.json", "w") as outfile:
 with open("/work/phenophasebbn/bbn/season_map.json", "w") as outfile:
     json.dump(season_map, outfile)
 print("Finished writing metadata for encoding categoricals...")
+
+print("Begin embedding expert knowledge into DAG...")
+
+#create empty list for DAG
+wl = []
+#dummy string
+gen = "genotype"
+
+#dummy list of fake gene names
+ng = ["gen1", "gen2", "gen3"]
+
+for i in range(len(ng)):
+    temp = [gen, ng[i]]
+    wl.append(temp)
+
+#append season and growth rate
+wl.append(["season", "gr"])
+#create list of tuples
+wl_tup = list(map(tuple, wl))
+
+print(wl_tup)
+#create blacklist in similar fashion
+bl = [["season", "genotype"]]
+
+for x in range(len(ng)):
+    temp2 = [ng[i], "season"]
+    bl.append(temp2)
+bl_tup = list(map(tuple, bl))
+print(bl_tup)
+
+
+
 # learn structure with NOTEARS, over 1000 iterations,and keep edge weights > 0.95
-device = torch.cuda.is_available()
-print('GPU is available:', device)
+#device = torch.cuda.is_available()
+#print('GPU is available:', device)
 
-print("Starting NO TEARS DAG structure learning...")
-from causalnex.structure.notears import from_pandas
+#print("Starting NO TEARS DAG structure learning...")
+#from causalnex.structure.notears import from_pandas
 
-sm = from_pandas(X=dum_df, max_iter=10, w_threshold=0.95)
+#sm = from_pandas(X=dum_df, max_iter=10, w_threshold=0.95)
 
 
-print("Finished structure learning...begin pickling structure model.")
-#pickle the structure model
-import pickle
-# make pickle file binary
-smp = open("/work/phenophasebbn/bbn/nt_sm", "wb")
+#print("Finished structure learning...begin pickling structure model.")
+##pickle the structure model
+#import pickle
+## make pickle file binary
+#smp = open("/work/phenophasebbn/bbn/nt_sm", "wb")
 # dump the pickle; syntax = (model, filename)
-pickle.dump(sm, smp)
+#pickle.dump(sm, smp)
 # close the pickle
-smp.close()
+#smp.close()
 
-print("Generating image of DAG...")
+#print("Generating image of DAG...")
 #output plot of learned graph
 # no need to apply thresholding, since this is taken care of in the sm with w_threshold
-from causalnex.plots import plot_structure
-viz = plot_structure(sm)
-viz.draw("/work/phenophasebbn/bbn/sm_plot.png")
+#from causalnex.plots import plot_structure
+#viz = plot_structure(sm)
+#viz.draw("/work/phenophasebbn/bbn/sm_plot.png")
 
